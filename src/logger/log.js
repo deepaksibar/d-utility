e8c3c067cc0567775b24326bb36a219f54fd6bec
Deepak Kumar <deepaksibar@gmail.com>
2016-08-27 14:42:20 +0530

    Update log.js

--- src/logger/log.js
+++ src/logger/log.js
@@ -1,1 +1,10 @@
-
+log = function(msg) {
+  var d = new Date();
+  var time = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+  if (typeof copy != "undefined") {
+    copy(msg);
+  }
+  if (typeof console != "undefined") {
+    console.log("log: " + time + " - " + msg);
+  }
+}

f45aa2ba7c8d239c7237d3d7d3f5e31c995722f1
Deepak Kumar <deepaksibar@gmail.com>
2016-08-30 15:26:08 +0530

    Update logger.js

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -1,6 +1,7 @@
 (function (jsutility) {
   var logger = jsutility.logger = function(options) {
     this.debug = true;
+    this.title = "";
   }
   
   logger.prototype.log = function(msg) {
@@ -11,7 +12,7 @@
         copy(msg);
       }
       if (typeof console != "undefined") {
-        console.log("igv-canvas: " + time + " " + msg);
+        console.log(this.title + time + " " + msg);
       }
     }
   }

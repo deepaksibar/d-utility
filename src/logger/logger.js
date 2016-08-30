346b78838498c39a1c145f24ea0f9a973cd039ad
Deepak Kumar <deepaksibar@gmail.com>
2016-08-30 15:36:25 +0530

    Update logger.js

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -1,19 +1,17 @@
-(function (jsutility) {
-  var logger = jsutility.logger = function(options) {
-    this.debug = true;
-    this.title = "";
-  }
-  
-  logger.prototype.log = function(msg) {
-    if (this.debug) {
-      var d = new Date();
-      var time = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-      if (typeof copy != "undefined") {
-        copy(msg);
-      }
-      if (typeof console != "undefined") {
-        console.log(this.title + time + " " + msg);
-      }
+var logger = module.exports = function(options) {
+  this.debug = true;
+  this.title = "";
+}
+
+logger.prototype.log = function(msg) {
+  if (this.debug) {
+    var d = new Date();
+    var time = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+    if (typeof copy != "undefined") {
+      copy(msg);
+    }
+    if (typeof console != "undefined") {
+      console.log(this.title + time + " " + msg);
     }
   }
-})(window.jsutility || (window.jsutility = {}));
+}

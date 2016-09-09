b384180f88f6adc9cfd19a9e1cf54cc0e08fdb88
Deepak Kumar <deepaksibar@gmail.com>
2016-09-09 17:34:44 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,44 @@
-var gulp = require('gulp');
+var gulp = require("gulp");
 var concat = require('gulp-concat');  
 var rename = require('gulp-rename');  
 var uglify = require('gulp-uglify'); 
+var less = require('gulp-less');
 
-var jsFiles = 'src/**/*.js',  
-    jsDest = 'dist';
+const del = require('del');
+const optimize = true;
 
-gulp.task('dist', function() {  
-    return gulp.src(jsFiles)
-        .pipe(concat('jsutility.js'))
-        .pipe(gulp.dest(jsDest))
-		.pipe(rename('jsutility.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest(jsDest));
+const PATHS = {
+    APP: "src/**/*.js",
+    CSS: 'src/**/*.css',
+	IMAGES: 'src/images/**/*',
+    BUILD: "./dist",
+    DEST_APP:'./dist',
+    DEST_CSS:'./dist/styles',
+	DEST_IMAGES:'./dist/images'
+};
+
+gulp.task('clean-build-dir', function () {
+    return del(PATHS.BUILD + '/**/*', {force: true});
 });
+
+gulp.task("concat-uglify-js", ['clean-build-dir'], function () {
+    return gulp.src(PATHS.APP)
+        .pipe(concat('igv.js'))
+        .pipe(gulp.dest(PATHS.DEST_APP))
+		.pipe(rename('igv.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(PATHS.DEST_APP));
+});
+
+gulp.task("compile-less", ['clean-build-dir'], function () {
+    return gulp.src(PATHS.CSS)
+        .pipe(less({compress: optimize}))
+        .pipe(gulp.dest(PATHS.DEST_CSS));
+});
+
+gulp.task("copy-images", ['clean-build-dir'], function () {
+    return gulp.src(PATHS.IMAGES)
+        .pipe(gulp.dest(PATHS.DEST_IMAGES));
+});
+
+gulp.task("default", ["clean-build-dir", "concat-uglify-js", "compile-less", "copy-images"]);
